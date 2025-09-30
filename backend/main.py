01f1d1323513d6b803eff490296881aa747f04f1
ali aghaie <4liaghaie@gmail.com>
2025-09-30 19:58:32 +0200

    cors5

--- backend/main.py
+++ backend/main.py
@@ -72,6 +72,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://89.116.157.224:3000",   
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
